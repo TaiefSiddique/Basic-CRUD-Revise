82b329601ec871a08348ac36148d373d9d6be086
TaiefSiddique <taiefsiddik2@gmail.com>
2024-01-28 22:35:21 +0600

    Initialization

---
+++ client/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
+
+// import { defineConfig } from 'tailwindcss';
+// import daisyui from 'daisyui';
+
+// export default defineConfig({
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [daisyui],
+// });
